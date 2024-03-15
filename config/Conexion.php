9faab7c3e6ff6bdae928f9baa7d73a6671d1c930
Elvis Pachacama <davis-elvis@hotmail.com>
2024-03-15 16:07:36 -0500

    Conexion

---
+++ config/Conexion.php
@@ -0,0 +1,48 @@
+<?php
+require_once "global.php";
+
+//Steamos las variables de conexión
+$conexion= new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+//Realizamos la conexión
+mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
+
+//si tenemos un posible error en la conexión lo mostramos
+
+if (mysqli_connect_errno()) {
+    //Imprimimos el error posible que de 
+    printf("Fallo la conexión a la base de datos; %\n", mysqli_connect_error());
+    exit();
+}
+if (!function_exists('ejecutarConsulta')) {
+    # code...
+    function ejecutarConsulta($sql)
+    {
+        global $conexion;
+        $query=$conexion->query($sql);
+        return $query;
+    }
+
+    //creamos una funcion para realizar una consulta de una sola fila
+    function ejecutarCOnsultaSImpleFila($sql){
+        global $conexion;
+        $query = $conexion->query($sql);
+        return $query;
+    }
+
+    //Implememtamos un función para retornar el id de un registro
+    function ejecutarCOnsulta_retornarID($sql)
+    {
+        global $conexion;
+        $query = $conexion->query($sql);
+        return $conexion->insert_id;
+    }
+    //función para limpiar uan cadena 
+    function limpiarCadena($str)
+    {
+        global $conexion;
+        
+    }
+
+}
+?>
---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//ip de la pc servidor de la base dee datos
+define("DB_HOST", "localhost");
+//NOmbre de la base de datos
+
+define("DB_NAME","mobile");
+
+//usuario de la base de datos
+define("DB_USERNAME", "root");
+
+//contraseña de la base de datos
+define("DB_PASSWORD","");
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE", "utf8");
+
+//define ina constante como nombre delproyecto
+define("PRO_NOMBRE", "PhoneLab")
+?>
